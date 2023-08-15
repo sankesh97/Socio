eaa04e3ddfe171714f6f43d11dd02d9f0f1fc84e
sankesh97 <ramojitinorange@gmail.com>
2023-08-15 11:43:21 +0530

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,14 +2,14 @@ import { AuthProvider, AuthContext } from './AuthContext';
 import { UsersProvider, UsersContext } from './UsersContext';
 import { PostsProvider, PostsContext } from './PostsContext';
 
-export { AuthContext, UsersContext, PostsContext };
-
 export const AppProvider = ({ children }) => {
   return (
     <AuthProvider>
       <UsersProvider>
-        <PostsProvider> {children}</PostsProvider>
+        <PostsProvider>{children}</PostsProvider>
       </UsersProvider>
     </AuthProvider>
   );
 };
+
+export { AuthContext, UsersContext, PostsContext };
