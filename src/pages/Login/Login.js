81a302e53c7cb6abafc4dcc2a8fe8dbadb56a2ba
sankesh97 <ramojitinorange@gmail.com>
2023-08-26 16:59:50 +0530

    Formik Setup

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,73 +1,79 @@
-import { useFormik } from 'formik';
+import { Field, Formik, useFormik, Form } from 'formik';
 import { useContext } from 'react';
 import * as Yup from 'yup';
 
 import { AuthContext } from '../../context/AuthContext';
 import Button from '../../components/Button/Button';
+import { Row } from 'react-bootstrap';
 
 const Login = () => {
   const { loginHandler } = useContext(AuthContext);
 
-  const formik = useFormik({
-    initialValues: {
-      userName: '',
-      password: '',
-    },
-    validationSchema: Yup.object({
-      userName: Yup.string().required('Required'),
-      password: Yup.string()
-        .required('Password is a required field')
-        .min(3, 'Password must be at least 8 characters'),
-    }),
-    onSubmit: (values) => {
-      loginHandler(values);
-    },
+  const loginSchema = Yup.object().shape({
+    userName: Yup.string().required('UserName is Required'),
+    password: Yup.string().required('Password is Required'),
   });
 
   return (
     <div className='col-md p-5 d-flex flex-column justify-content-center '>
       <h1 className='fw-bold text-center text-teal'>Socio</h1>
       <h4 className='text-teal text-center'> Login </h4>
-      <br />
-      <div className='d-flex justify-content-center'>
-        <form onSubmit={formik.handleSubmit}>
-          <div className='mb-3'>
-            <label htmlFor='InputEmail' className='form-label'>
-              Username
-            </label>
-            <input
-              type='text'
-              className='form-control'
-              id='InputEmail'
-              aria-describedby='EmailHelp'
-              {...formik.getFieldProps('userName')}
-            />
-            <div id='EmailHelp' className='form-text'>
-              {formik.touched.email && formik.errors.email ? (
-                <div>{formik.errors.email}</div>
-              ) : null}
+      <Formik
+        validateOnBlur={false}
+        validateOnChange={false}
+        initialValues={{
+          userName: '',
+          password: '',
+        }}
+        validationSchema={loginSchema}
+        onSubmit={(values) => {
+          loginHandler(values);
+        }}
+      >
+        {({ errors, touched, values, handleChange }) => (
+          <Form>
+            <div className='mb-3'>
+              <label htmlFor='userName' className='form-label'>
+                userName
+              </label>
+              <input
+                type='text'
+                name='userName'
+                id='userName'
+                value={values.userName}
+                onChange={handleChange}
+                isValid={touched.userName && !errors.userName}
+                className='form-control'
+                aria-describedby='userNameHelpBlock'
+              />
+              <div id='userNameHelpBlock' className='form-text'>
+                {errors.userName}
+              </div>
             </div>
-          </div>
-          <div className='mb-3'>
-            <label htmlFor='InputPassword' className='form-label'>
-              Password
-            </label>
-            <input
-              type='password'
-              className='form-control'
-              id='InputPassword'
-              aria-describedby='PasswordHelp'
-              {...formik.getFieldProps('password')}
-            />
-          </div>
-          <div id='PasswordHelp' className='form-text'>
-            {formik.touched.password && formik.errors.password
-              ? formik.errors.password
-              : null}
-          </div>
-          <Button type='submit'>Submit</Button>
-        </form>
-      </div>
+
+            <div className='mb-3'>
+              <label htmlFor='password' className='form-label'>
+                Password
+              </label>
+              <input
+                name='password'
+                type='password'
+                id='password'
+                className='form-control'
+                value={values.password}
+                onChange={handleChange}
+                isValid={touched.password && !errors.password}
+                aria-describedby='passwordHelpBlock'
+              />
+              <div id='passwordHelpBlock' className='form-text'>
+                {errors.password}
+              </div>
+            </div>
+            <Button type='submit'>Submit</Button>
+          </Form>
+        )}
+      </Formik>
+      <br />
     </div>
   );
 };
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,26 +1,29 @@
 import { useContext, useState } from 'react';
-
+import * as Yup from 'yup';
 import Button from '../../components/Button/Button';
 import { AuthContext } from '../../context/AuthContext';
 import Toaster from '../../components/Toaster';
+import { Form, Formik } from 'formik';
 
 const Register = () => {
   const { signupHandler } = useContext(AuthContext);
-  const [formValues, setFormValues] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    userName: '',
-    password: '',
-    confirmPassword: '',
-  });
-  const [showPass, setShowPass] = useState(false);
 
-  const onChangeHandler = (id, value) => {
-    setFormValues((prevState) => {
-      return { ...prevState, [id]: value };
-    });
-  };
+  const registerSchema = Yup.object().shape({
+    firstName: Yup.string().required('First Name is Required'),
+    lastName: Yup.string().required('Last Name is Required'),
+    email: Yup.string()
+      .required('Email is Required')
+      .email('Must be a valid email address'),
+    userName: Yup.string().required('User Name is Required'),
+    password: Yup.string()
+      .required('Password is Required')
+      .min(5, 'Your password is too short.'),
+    confirmPassword: Yup.string()
+      .required('Confirm Password is Required')
+      .oneOf([Yup.ref('password')], 'Passwords do not match!'),
+  });
+
+  const [showPass, setShowPass] = useState(false);
 
   return (
     <div className='col-md p-5 d-flex flex-column justify-content-center '>
@@ -28,129 +31,168 @@ const Register = () => {
       <h4 className='text-teal text-center'> Register </h4>
       <br />
       <div className='d-flex justify-content-center'>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            formValues.password !== formValues.confirmPassword
-              ? Toaster(
-                  'ERROR',
-                  'Password Mis-Match. Please check your passwords'
-                )
-              : signupHandler(formValues);
+        <Formik
+          validateOnBlur={false}
+          validateOnChange={false}
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+            userName: '',
+            password: '',
+            confirmPassword: '',
+          }}
+          validationSchema={registerSchema}
+          onSubmit={(values) => {
+            signupHandler(values);
           }}
         >
-          {/* First Name */}
-          <div className='mb-3'>
-            <label htmlFor='firstName' className='form-label'>
-              First Name
-            </label>
-            <input
-              type='text'
-              className='form-control'
-              name='firstName'
-              id='firstName'
-              onChange={(event) => {
-                onChangeHandler(event.target.id, event.target.value);
-              }}
-              required
-            />
-          </div>
-          {/* Last Name */}
-          <div className='mb-3'>
-            <label htmlFor='lastName' className='form-label'>
-              Last Name
-            </label>
-            <input
-              type='text'
-              className='form-control'
-              id='lastName'
-              onChange={(event) => {
-                onChangeHandler(event.target.id, event.target.value);
-              }}
-              required
-            />
-          </div>
-          {/* User Name */}
-          <div className='mb-3'>
-            <label htmlFor='UserName' className='form-label'>
-              User Name
-            </label>
-            <input
-              type='text'
-              className='form-control'
-              id='userName'
-              onChange={(event) => {
-                onChangeHandler(event.target.id, event.target.value);
-              }}
-              required
-            />
-          </div>
-          {/* Email */}
-          <div className='mb-3'>
-            <label htmlFor='InputEmail' className='form-label'>
-              Email address
-            </label>
-            <input
-              type='email'
-              className='form-control'
-              id='email'
-              onChange={(event) => {
-                onChangeHandler(event.target.id, event.target.value);
-              }}
-              aria-describedby='EmailHelp'
-            />
-          </div>
-          {/* Password */}
-          <label htmlFor='password' className='form-label'>
-            Password
-          </label>
-          <div className='mb-3 input-group'>
-            <input
-              type={showPass ? 'text' : 'password'}
-              className='form-control'
-              id='password'
-              onChange={(event) => {
-                onChangeHandler(event.target.id, event.target.value);
-              }}
-            />
-            <span
-              className='input-group-text'
-              onClick={() => setShowPass(!showPass)}
-            >
-              {showPass ? (
-                <i className='bi bi-eye-slash'></i>
-              ) : (
-                <i className='bi bi-eye'></i>
-              )}
-            </span>
-          </div>
-          {/* Confirm Password */}
-          <label htmlFor='confirmPassword' className='form-label'>
-            Confirm Password
-          </label>
-          <div className='mb-3 input-group'>
-            <input
-              type={showPass ? 'text' : 'password'}
-              className='form-control'
-              id='confirmPassword'
-              onChange={(event) => {
-                onChangeHandler(event.target.id, event.target.value);
-              }}
-            />
-            <span
-              className='input-group-text'
-              onClick={() => setShowPass(!showPass)}
-            >
-              {showPass ? (
-                <i className='bi bi-eye-slash'></i>
-              ) : (
-                <i className='bi bi-eye'></i>
-              )}
-            </span>
-          </div>
+          {({ errors, touched, values, handleChange }) => (
+            <Form>
+              {/* First Name */}
+              <div className='mb-3'>
+                <label htmlFor='firstName' className='form-label'>
+                  First Name
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  name='firstName'
+                  id='firstName'
+                  value={values.firstName}
+                  onChange={handleChange}
+                  isValid={touched.firstName && !errors.firstName}
+                  aria-describedby='firstNameHelpBlock'
+                  required
+                />
+                <div id='firstNameHelpBlock' className='form-text'>
+                  {errors.firstName}
+                </div>
+              </div>
+              {/* Last Name */}
+              <div className='mb-3'>
+                <label htmlFor='lastName' className='form-label'>
+                  Last Name
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='lastName'
+                  value={values.lastName}
+                  onChange={handleChange}
+                  isValid={touched.lastName && !errors.lastName}
+                  aria-describedby='lastNameHelpBlock'
+                  required
+                />
+                <div id='lastNameHelpBlock' className='form-text'>
+                  {errors.lastName}
+                </div>
+              </div>
+              {/* User Name */}
+              <div className='mb-3'>
+                <label htmlFor='UserName' className='form-label'>
+                  User Name
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='userName'
+                  value={values.userName}
+                  onChange={handleChange}
+                  isValid={touched.userName && !errors.userName}
+                  aria-describedby='userNameHelpBlock'
+                  required
+                />
+                <div id='userNameHelpBlock' className='form-text'>
+                  {errors.userName}
+                </div>
+              </div>
+              {/* Email */}
+              <div className='mb-3'>
+                <label htmlFor='InputEmail' className='form-label'>
+                  Email address
+                </label>
+                <input
+                  type='email'
+                  className='form-control'
+                  id='email'
+                  value={values.email}
+                  onChange={handleChange}
+                  isValid={touched.email && !errors.email}
+                  aria-describedby='emailHelpBlock'
+                  required
+                />
+                <div id='emailHelpBlock' className='form-text'>
+                  {errors.email}
+                </div>
+              </div>
+              {/* Password */}
+              <label htmlFor='password' className='form-label'>
+                Password
+              </label>
+              <div className='mb-3'>
+                <div className='input-group'>
+                  <input
+                    type={showPass ? 'text' : 'password'}
+                    className='form-control'
+                    id='password'
+                    value={values.password}
+                    onChange={handleChange}
+                    isValid={touched.password && !errors.password}
+                    aria-describedby='passwordHelpBlock'
+                    required
+                  />
+                  <span
+                    className='input-group-text'
+                    onClick={() => setShowPass(!showPass)}
+                  >
+                    {showPass ? (
+                      <i className='bi bi-eye-slash'></i>
+                    ) : (
+                      <i className='bi bi-eye'></i>
+                    )}
+                  </span>
+                </div>
+                <div id='passwordHelpBlock' className='form-text'>
+                  {errors.password}
+                </div>
+              </div>
+              {/* Confirm Password */}
+              <label htmlFor='confirmPassword' className='form-label'>
+                Confirm Password
+              </label>
+              <div className='mb-3'>
+                <div className='input-group'>
+                  <input
+                    type={showPass ? 'text' : 'password'}
+                    className='form-control'
+                    id='confirmPassword'
+                    value={values.confirmPassword}
+                    onChange={handleChange}
+                    isValid={touched.confirmPassword && !errors.confirmPassword}
+                    aria-describedby='confirmPasswordHelpBlock'
+                    required
+                  />
+                  <span
+                    className='input-group-text'
+                    onClick={() => setShowPass(!showPass)}
+                  >
+                    {showPass ? (
+                      <i className='bi bi-eye-slash'></i>
+                    ) : (
+                      <i className='bi bi-eye'></i>
+                    )}
+                  </span>
+                </div>
+                <div id='confirmPasswordHelpBlock' className='form-text'>
+                  {errors.confirmPassword}
+                </div>
+              </div>
 
-          <Button type='submit'>Submit</Button>
-        </form>
+              <Button type='submit'>Submit</Button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
